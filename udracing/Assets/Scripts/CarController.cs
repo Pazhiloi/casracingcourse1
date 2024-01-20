b6fa4e309c0da127d977d18f486705501ad4f809
Miroslav Miroslavov <martinsen06@gmail.com>
2024-01-20 08:57:42 +0200

    5 Controlling Acceleration

--- udracing/Assets/Scripts/CarController.cs
+++ udracing/Assets/Scripts/CarController.cs
@@ -7,14 +7,29 @@ public class CarController : MonoBehaviour
   public Rigidbody theRB;
   public float maxSpeed = 30f;
 
+  public float forwardAccel = 8f, reverseAccel = 4f;
+
+  private float speedInput;
+
   private void Start() {
     theRB.transform.parent = null;
   }
 
   private void Update() {
-    theRB.AddForce(new Vector3(0, 0, 100f));
+    speedInput = 0f;
 
+    if (Input.GetAxis("Vertical") > 0)
+    {
+      speedInput = Input.GetAxis("Vertical") * forwardAccel;
+    }else if (Input.GetAxis("Vertical") < 0)
+    {
+      speedInput = Input.GetAxis("Vertical") * reverseAccel;
+    }
     transform.position = theRB.position;
+  }
+
+  private void FixedUpdate() {
+    theRB.AddForce(new Vector3(0, 0, speedInput));
   }
 }
  
