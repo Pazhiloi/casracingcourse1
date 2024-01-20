5b502bcca9d1a7acb211e061b98aa88308b874e7
Miroslav Miroslavov <martinsen06@gmail.com>
2024-01-20 11:13:59 +0200

    9 setting our maximum speed

--- udracing/Assets/Scripts/CarController.cs
+++ udracing/Assets/Scripts/CarController.cs
@@ -45,5 +45,10 @@ public class CarController : MonoBehaviour
   private void FixedUpdate()
   {
     theRB.AddForce(transform.forward * speedInput * 1000f);
+
+    if (theRB.velocity.magnitude > maxSpeed)
+    {
+      theRB.velocity = theRB.velocity.normalized * maxSpeed;
+    }
   }
 }
