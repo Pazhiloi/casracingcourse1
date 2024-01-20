0f4f77438c7550de31e3e76860f9822dedc3e51a
Miroslav Miroslavov <martinsen06@gmail.com>
2024-01-20 08:36:16 +0200

    3 First script basics

---
+++ udracing/Assets/Scripts/CarController.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+  public Rigidbody theRB;
+  public float maxSpeed = 30f;
+}
