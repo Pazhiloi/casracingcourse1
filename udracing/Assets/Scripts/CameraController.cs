3b209b9dd5e5ed3a9eb9ad2d5e6da4a6f234f744
Miroslav Miroslavov <martinsen06@gmail.com>
2024-01-22 10:56:14 +0200

    19 Top-Down Camera

---
+++ udracing/Assets/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public CarController target;
+    private Vector3 offsetDir;
+
+    public float minDistance, maxDistance;
+    private float activeDistance;
+
+
+    private void Start() {
+      offsetDir  = transform.position - target.transform.position;
+
+      activeDistance = minDistance;
+
+      offsetDir.Normalize();
+    }
+
+    private void Update() {
+      activeDistance = minDistance + ((maxDistance - minDistance) * (target.theRB.velocity.magnitude / target.maxSpeed));
+      transform.position = target.transform.position + (offsetDir * activeDistance);
+    }
+}
